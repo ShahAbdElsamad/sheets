ce00ea7a550a17bcc1210eab4120bab66b70aabb
unknown <shahd010220@gmail.com>
2024-12-03 21:34:26 +0200

    sheet1

---
+++ Sheet 1-2.py
@@ -0,0 +1,192 @@
+#Sheet (1)
+#x='shahd'
+#print('Hello',x)  
+
+
+#y=56,7
+ #y=float(input('Enter your y'))
+
+
+#z=145
+#z=int(input('Enter your z'))
+
+#s=shahd
+#s=str(input('Enter your s'))
+
+#passward=91
+#passward=float(input('Enter y'))our p
+
+
+# x=float(passord)
+
+#No
+
+#nambar=[1,2,3,4,5]
+
+#namber=(10,11,12,13,14,15)
+
+#y=[1,2,3,4]     y=(1,2,3,4)
+
+#s={5,7,8}
+
+#No
+
+#لغه بايثون خليط بين مفسره ومترجمه ف هي تترجم كود بايت قبل تفسيره
+
+# لغات المستوى المنخفض تُستخدم عند الحاجة إلى أداء عالٍ وتحكم دقيق.  
+ #لغات المستوى العالي تُستخدم لتطوير التطبيقات بسرعة وسهولة.
+
+
+
+
+# =علامه......== اداه مقارنه
+
+
+#لاتساوى
+
+#x=10
+#x=10+15=20
+#y=10/5
+#print(y)
+#s=10*10
+#print(s)
+#h=10**3
+#print(h)
+#x=11**4
+#print(x)
+#=2**3
+#print(a)
+#f=11/3
+# yeas namber=3.66
+#g=int(namber)
+
+
+#rint(g)
+#e=10>15
+#print(e)
+            
+#تستخدم لما تحب تتاكد ان كل العناصر صحيحه حتى لو عنصر واحد ترجع غلط (ALL)
+#لو القاىمه فاضيه  True
+#لو النص فارغ بدينا False
+#ف القاموس بيشيك على المفتاح لو بصفر بيدينا False
+#لو حتى عنصر واحد فقط غلط بيدينا FALSE
+
+#IF لفحص الشرط الاساسى 
+#ELIF لفحص ال شروط الاضافيه اذا كان الشرط الاساسى خاطا
+
+#IF لفحص الشرط الاساسى
+#else   لتنفيذ كود بديل اذا لم يتحقق اى شرط
+ 
+ #نعم يمكن استخدامها اكثر من مرا ف نفس الكود واول شرط صحيح يتم تنفيذه
+
+#NO    لا تحتاج الى شرط فهى تعمل اذا كانت كل الشروط السابقه خطا  
+
+#لا لن يتم التحقق من جميع الشروط بمجرد من اى يتحقق من شرط واحد يتوقف عن 
+
+#انعم ليس شرطا اساسيا وجودها وانه اختيارىelse
+
+
+
+#my_list = [2,4, 6, 8, 10]
+
+#if 4 in my_list:
+#    print("yes")
+#else:
+#    print("No")
+    
+
+#my_list = [2,4, 6, 8, 10]
+#if 4 in  my_list and  6 in  my_list:
+#    print (4,6)
+#else:
+#    print ('NO')
+
+#my_list=[2,4,6,8,10]
+#if 3 in my_list or 6 in my_list:
+#    print (3,6)
+#else:
+#    print ('No')
+
+#my_list = [2,4, 6, 8, 10]
+#if 2 in my_list and 4 in my_list and 5 in my_list:
+#    print (2,4,5)
+#else:
+#    print("No") 
+# 
+# 
+#    
+#Sheet(2)
+
+#units =int(input("ادخل عدد الوحدات المستهلكه:"))
+#if units <= 100:
+#    bill=0
+#elif units <= 200:
+#     bill=(units-100) * 5
+#else:
+#     bill =(100 * 5)+((units-200) * 10)
+#print (f"إجمالي الفاتورة هو: Rs {bil}")
+
+costprice =float(input("ادخل سعر تكلفه الدراجه الناريه:"))
+if costprice > 100000:
+    tax =costprice * 0.15
+elif costprice > 50000 and costprice <= 100000:
+    tax =costprice * 0.1
+else:         
+    tax =costprice * 0.05
+print(f"ضريبه الطرق التى يجب فعلها هى: Rs {tax}")
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+#num1 = int(input("أدخل العدد الأول: "))
+#num2 = int(input("أدخل العدد الثاني: ")) 
+#largest = num1 if num1 > num2 else num2
+#print(f"العدد الأكبر هو: {largest}")
+#حل اخر لنفس السؤال
+#num1=float(input("ادخل الرقم الاول:"))
+#num2=float (input(" ادخل الرقم الثانى:"))
+#largest=num1 if num1 > num2 else num2 
+#print(f"العدد الأكبر هو: {largest}")
+
+#num=float(input("ادخل الرقم:"))
+#resuit=  "الموجب" if num > 0 else "سالب"
+#print(f"الرقم {num} هو: {resuit}")
+
+#x=int(input("Enter yuor numder:" ))
+#print ("Even")if x % 2==0 else print ("odd")
+
+
+#total_days =int(input(" ادخل عدد ايام العمل الكلى: "))
+#absent_days=int(input("ادخل عدد ايام الذى غاب فيها الطالب: "))
+#attended_days= total_days - absent_days
+#attendance_percentage= (attended_days / total_days)*100
+#if attendance_percentage < 75:
+#    print(f"نسبة الحضور: {attendance_percentage:.2f}%  -   الطالب غير مسموح له بالجلوس في الامتحان.")
+#else:
+
+#   print(f"نسبة الحضور: {attendance_percentage:.2f}% -  الطالب مسموح له بالجلوس فى الامتحان.")
+ 
+#percentage = float(input("أدخل النسبة المئوية: "))
+#if percentage < 25:
+#    grade = "D"
+#elif 25 <= percentage < 45:
+#    grade = "C"
+#elif 45 <= percentage < 50:
+#    grade = "B"
+#elif 50 <= percentage < 60:
+#    grade = "B+"
+#elif 60 <= percentage <= 80:
+#    grade = "A"
+#else:   
+#    grade = "A+"
+#print("الدرجة:", grade)
